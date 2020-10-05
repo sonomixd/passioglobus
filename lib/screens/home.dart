dbe80574291c879529e574e2325ad1f917a799bb
sokolikonomi <sokol_ikonomi@yahoo.com>
2020-10-05 16:20:27 +0200

    added home screen

---
+++ lib/screens/home.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:passioglobus/blocs/auth_bloc.dart';
+import 'package:passioglobus/screens/login.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  StreamSubscription<User> homeStateSubsribtion;
+
+  @override
+  void initState() {
+    var authBloc = Provider.of<AuthBloc>(context, listen: false);
+    homeStateSubsribtion = authBloc.currentUser.listen((fbUser) {
+      if (fbUser == null) {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => LoginScreen()));
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    homeStateSubsribtion.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var authBloc = Provider.of<AuthBloc>(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SignInButton(Buttons.Facebook,
+                text: 'Logout from facebook',
+                onPressed: () => authBloc.logout()),
+          ],
+        ),
+      ),
+    );
+  }
+}
